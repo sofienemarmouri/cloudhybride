db2ae82460e824b63a3eff0a36b6ea15d2b7a8b6
sofienemarmouri <48206641+sofienemarmouri@users.noreply.github.com>
2023-11-02 17:22:27 +0100

    Update aws-subnet-mgmt-01.tf

--- vpc/mgmtvpc/bsubnet/aws-subnet-mgmt-01.tf
+++ vpc/mgmtvpc/bsubnet/aws-subnet-mgmt-01.tf
@@ -1,35 +1,29 @@
-provider "aws" {
-  access_key = "AKIA57K4OIW7NOBQMXXP"
-  secret_key = "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"
-  region     = "eu-west-3"
-}
-
-resource "aws_subnet" "onx-aws-subnet-mgmt-01" {
+resource "aws_subnet" "aws-subnet-mgmt-01" {
   vpc_id                  = "vpc-0317680e9bab9e292"
   cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-3a"
 tags = {
-   Name = "onx-aws-subnet-mgmt-01"
+   Name = "aws-subnet-mgmt-01"
 }
 }
 
-resource "aws_subnet" "onx-aws-subnet-mgmt-02" {
+resource "aws_subnet" "aws-subnet-mgmt-02" {
   vpc_id                  = "vpc-0317680e9bab9e292"
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-3a"
 tags = {
-   Name = "onx-aws-subnet-mgmt-02"
+   Name = "aws-subnet-mgmt-02"
 }
 }
 
-resource "aws_subnet" "onx-aws-subnet-mgmt-03" {
+resource "aws_subnet" "aws-subnet-mgmt-03" {
   vpc_id                  = "vpc-0317680e9bab9e292"
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-3a"
 tags = {
-   Name = "onx-aws-subnet-mgmt-03"
+   Name = "aws-subnet-mgmt-03"
 }
 }
