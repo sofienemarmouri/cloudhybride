05cc083839fb6cf9b6460a8e3aa21eee2e57d60b
sofienemarmouri <48206641+sofienemarmouri@users.noreply.github.com>
2023-11-02 17:25:37 +0100

    Update aws-dev-vpn-sts-mgmt-01.tf

--- vpc/vpn/sts/aws-dev-vpn-sts-mgmt-01.tf
+++ vpc/vpn/sts/aws-dev-vpn-sts-mgmt-01.tf
@@ -1,29 +1,23 @@
-provider "aws" {
-  access_key = "AKIA57K4OIW7NOBQMXXP"
-  secret_key = "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"
-  region     = "eu-west-3"
-}
-
 #virtual private gateways and attached to vpc
-resource "aws_vpn_gateway" "onx-aws-dev-vpgw-mgmt-01" {
+resource "aws_vpn_gateway" "aws-dev-vpgw-mgmt-01" {
   vpc_id            = "vpc-0317680e9bab9e292"
   tags = {
-    Name = "onx-aws-dev-vpgw-mgmt-01"
+    Name = "aws-dev-vpgw-mgmt-01"
   }
 }
 
 #customer gateway
-resource "aws_customer_gateway" "onx-aws-dev-cgw-mgmt-01" {
+resource "aws_customer_gateway" "aws-dev-cgw-mgmt-01" {
   type        = "ipsec.1"
   bgp_asn     = 65000
-  ip_address  = "185.202.52.227" #On Premise public IP.
+  ip_address  = "xxx.xx.xxx.xx" #On Premise public IP.
   tags = {
-    Name = "onx-aws-dev-cgw-mgmt-01"
+    Name = "aws-dev-cgw-mgmt-01"
   }
 }
 
 #vpn_connection
-resource "aws_vpn_connection" "onx-aws-dev-vpn-connection-mgmt-01" {
+resource "aws_vpn_connection" "aws-dev-vpn-connection-mgmt-01" {
   customer_gateway_id   = "cgw-03dc9dfed52ec7f6d"
   vpn_gateway_id        = "vgw-0ca76d44f075f8626"
   type              = "ipsec.1"
@@ -48,7 +42,7 @@ resource "aws_vpn_connection" "onx-aws-dev-vpn-connection-mgmt-01" {
 #  tunnel2_phase2_lifetime_seconds         = "3600"
 
   tags = {
-    Name = "onx-aws-dev-vpn-connection-mgmt-01"
+    Name = "aws-dev-vpn-connection-mgmt-01"
   }
 }
 #
