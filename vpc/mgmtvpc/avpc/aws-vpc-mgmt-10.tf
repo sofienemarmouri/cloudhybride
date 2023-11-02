e5417632f2924cdec6d26466c9697ddf46ad60a0
sofienemarmouri <48206641+sofienemarmouri@users.noreply.github.com>
2023-11-02 17:21:52 +0100

    Update aws-vpc-mgmt-10.tf

--- vpc/mgmtvpc/avpc/aws-vpc-mgmt-10.tf
+++ vpc/mgmtvpc/avpc/aws-vpc-mgmt-10.tf
@@ -1,16 +1,10 @@
-provider "aws" {
-  access_key = "AKIA57K4OIW7NOBQMXXP"
-  secret_key = "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"
-  region     = "eu-west-3"
-}
-
-resource "aws_vpc" "onx-aws-vpc-mgmt-10" {
+resource "aws_vpc" "aws-vpc-mgmt-10" {
   cidr_block           = "10.0.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
 tags = {
-    Name = "onx-aws-vpc-mgmt-10"
+    Name = "aws-vpc-mgmt-10"
     mounting = "terraform"
     env = "mgmt"
     region = "paris"
