ec88ea7a3b5cf13f85895691778aaf8014d0b454
sofienemarmouri <48206641+sofienemarmouri@users.noreply.github.com>
2023-11-02 17:20:18 +0100

    Update aws-dev-prometheus-mgmt-01.tf

--- mgmt/prometheus/aws-dev-prometheus-mgmt-01.tf
+++ mgmt/prometheus/aws-dev-prometheus-mgmt-01.tf
@@ -1,22 +1,16 @@
-provider "aws" {
-  access_key = "AKIA57K4OIW7NOBQMXXP"
-  secret_key = "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"
-  region     = "eu-west-3"
-}
-
-resource "aws_instance" "onx-aws-dev-prometheus-mgmt-01" {
+resource "aws_instance" "aws-dev-prometheus-mgmt-01" {
   ami = "ami-008bcc0a51a849165"
   availability_zone = "eu-west-3b"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0c5ac6ab8cbe307f3"
-  key_name        		 = "onx-aws-key-smarmouri"
+  key_name        		 = "aws-key-smarmouri"
   monitoring             = true
   vpc_security_group_ids = [
     "sg-023300e3f405b9229",
   ]
   user_data = file("install_prometheus.sh")
   tags = {
-    Name = "onx-aws-dev-prometheus-mgmt-01"
+    Name = "aws-dev-prometheus-mgmt-01"
     mounting = "terraform"
     env = "mgmt"
     ami = "ubuntu2004"
@@ -26,8 +20,8 @@ resource "aws_instance" "onx-aws-dev-prometheus-mgmt-01" {
 
 #
 #Allow SSH and apache inbound traffic
-resource "aws_security_group" "onx-aws-sg-mgmt-02" {
-  name        = "onx-aws-sg-mgmt-02"
+resource "aws_security_group" "aws-sg-mgmt-02" {
+  name        = "aws-sg-mgmt-02"
   description = "Allow All"
   vpc_id      = "vpc-0317680e9bab9e292"
   ingress {
@@ -43,6 +37,6 @@ resource "aws_security_group" "onx-aws-sg-mgmt-02" {
     cidr_blocks     = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "onx-aws-sg-mgmt-02"
+    Name = "aws-sg-mgmt-02"
   }
 }
