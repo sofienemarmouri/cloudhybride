7e1ea292713fda4566962b21522f83e6884f7e18
sofienemarmouri <48206641+sofienemarmouri@users.noreply.github.com>
2023-11-02 17:24:19 +0100

    Update aws-internet-gateway-mgmt-01.tf

--- vpc/mgmtvpc/eig/aws-internet-gateway-mgmt-01.tf
+++ vpc/mgmtvpc/eig/aws-internet-gateway-mgmt-01.tf
@@ -1,17 +1,11 @@
-provider "aws" {
-  access_key = "AKIA57K4OIW7NOBQMXXP"
-  secret_key = "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"
-  region     = "eu-west-3"
-}
-
-resource "aws_internet_gateway" "onx-aws-internet-gateway-mgmt-01" {
+resource "aws_internet_gateway" "aws-internet-gateway-mgmt-01" {
  vpc_id = "vpc-0317680e9bab9e292"
  tags = {
-        Name = "onx-aws-internet-gateway-mgmt-01"
+        Name = "aws-internet-gateway-mgmt-01"
 }
 }
 
 #attachement au VPC 20
-resource "aws_vpc" "onx-aws-vpc-mgmt-01" {
+resource "aws_vpc" "aws-vpc-mgmt-01" {
   cidr_block = "10.0.0.0/16"
 }
